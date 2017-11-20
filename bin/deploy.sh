67c70e72c8d5f39bd113a4c02ec8409189aa78c4
Ryan Wholey <rjwholey@gmail.com>
2017-11-19 18:30:47 -0800

    deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#npm install
+npm install
 npm run build
 export DB_PORT=2000
 export PORT=4000
@@ -8,3 +8,5 @@ export PORT=4000
 node_modules/.bin/forever start --uid "rw-escape-db" -a server/dbServer.js
 node_modules/.bin/forever start --uid "rw-escape-app" -a server/server.js
 
+echo "DB server started at ${DB_PORT}"
+echo "HOST server started at ${PORT}"
